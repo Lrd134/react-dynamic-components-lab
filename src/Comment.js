a21d1f0a0e2b6709cba5fa3a0150d68c9bf31d6f
Lrd134 <larryc3200@gmail.com>
2021-10-30 04:34:33 -0400

    commit tree d494dc43741832941144825025c8217b16238856

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 //your code here
+import React, {Component} from React;
+
+class Comment extends Component {
+  render() {
+    return (
+      <div>{this.props.commentText}</div>
+    )
+  }
+}
+
+export default Comment;
